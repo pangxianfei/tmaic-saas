47c59b2151b06f5fda9ecdda377586bd531f051e
PangXianFei <421339244@qq.com>
2023-02-24 02:50:26 +0800

    JWT及TOKEN模块开发

--- app/http/middleware/LoginMiddleware.go
+++ app/http/middleware/LoginMiddleware.go
@@ -5,21 +5,17 @@ import (
 	"github.com/kataras/iris/v12/context"
 	"tmaic/app/http/middleware/response"
 	"tmaic/app/services"
-	"tmaic/vendors/framework/helpers/debug"
 )
 
-// Login 获取JWT中的用户数据，封装成实体存放在ctx中供请求调用
 var Login context.Handler
 
 func initUserInfo() {
 	Login = func(ctx iris.Context) {
 		newUser := services.UserTokenService.GetUserInfo(ctx)
-		//debug.Dump("获取JWT中的用户数据，封装成实体存放在ctx中供请求调用")
 		if newUser == nil {
 			_ = ctx.JSON(response.ErrorTokenInvalidation())
 			return
 		}
-		debug.Dump(newUser)
 		ctx.Values().Set("UserInfo", newUser)
 		ctx.Next()
 	}

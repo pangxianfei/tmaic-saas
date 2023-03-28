55a94513f38d593938038f7c4e2b0167704cb349
PangXianFei <421339244@qq.com>
2023-03-29 03:10:32 +0800

    1.添加生产管理应用

---
+++ PdApp/http/controllers/v1/PdController.go
@@ -0,0 +1,32 @@
+package v1
+
+import (
+	"gitee.com/pangxianfei/framework/http/controller"
+	"gitee.com/pangxianfei/library/response"
+	"gitee.com/pangxianfei/saas/paas"
+)
+
+type PdController struct {
+	controller.BaseController
+}
+
+// PostInfo 获取当前登录用户
+func (c *PdController) PostInfo() *response.JsonResult {
+	return response.JsonData(paas.Auth.User(c.Context))
+}
+
+// PostStore 创建
+func (c *PdController) PostStore() *response.JsonResult {
+
+	return response.JsonCreateData("")
+}
+
+// PostEdit 修改用户资料
+func (c *PdController) PostEdit() *response.JsonResult {
+	return response.JsonSuccess()
+}
+
+// PostUpdate 更新
+func (c *PdController) PostUpdate() *response.JsonResult {
+	return response.JsonSuccess()
+}
---
+++ PdApp/routes/Api.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"gitee.com/pangxianfei/saas/middleware"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/mvc"
+
+	"tmaic/PdApp/http/controllers/v1"
+	PdAppMiddleware "tmaic/PdApp/http/middleware"
+)
+
+func PdAppApi(app *iris.Application) {
+	auth := app.Party("/")
+	auth.Use(middleware.LoginMiddleware(), PdAppMiddleware.PdAppMiddleware, middleware.Permissions)
+	mvc.Configure(auth, func(m *mvc.Application) {
+		m.Party("/pd").Handle(new(v1.PdController))
+
+	})
+}

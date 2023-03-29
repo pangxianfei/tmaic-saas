4a65a4e62feb6493808de16a9e2e7fb1bd41990b
PangXianFei <421339244@qq.com>
2023-03-30 02:19:04 +0800

    1.http请求响应关联上下文

--- UserApp/routes/Api.go
+++ UserApp/routes/Api.go
@@ -13,8 +13,8 @@ func UserAppApi(app *iris.Application) {
 	auth := app.Party("/")
 	auth.Use(middleware.LoginMiddleware(), UserAppMiddleware.UserAppMiddleware, middleware.Permissions)
 	mvc.Configure(auth, func(m *mvc.Application) {
-		m.Party("/user").Handle(new(v1.UserController)).Name = "user"
-		m.Party("/staff").Handle(new(v1.StaffController)).Name = "staff"
+		m.Party("/user").Handle(new(v1.UserController))
+		m.Party("/staff").Handle(new(v1.StaffController))
 		m.Party("/permission").Handle(new(v1.PermissionController))
 		m.Party("/role").Handle(new(v1.RoleController))
 		m.Party("/saas").Handle(new(v1.SaasController))
